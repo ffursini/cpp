b844ce0695433e6891fa499886445a770a08e1f9
fursini <francoursinikbw@gmail.com>
2024-05-29 14:39:34 +0200

    megaphone

--- cpp0/ex00/Megaphone.cpp
+++ cpp0/ex00/Megaphone.cpp
@@ -6,21 +6,16 @@
 /*   By: fursini <fursini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 22:40:56 by fursini           #+#    #+#             */
-/*   Updated: 2024/05/26 22:41:23 by fursini          ###   ########.fr       */
+/*   Updated: 2024/05/28 22:01:52 by fursini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-char	toupper(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 32);
-	return (c);
-}
+#include <string>
 
 int	main(int argc, char **argv)
 {
+	char c;
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
@@ -29,7 +24,10 @@ int	main(int argc, char **argv)
 	for (int i = 1; i < argc; i++)
 	{
 		for (int j = 0; argv[i][j]; j++)
-			std::cout << toupper(argv[i][j]);
+		{
+			c = toupper(argv[i][j]);
+			std::cout << c;
+		}
 	}
 	std::cout << std::endl;
 	return (0);
