d55c8c7d1493b93c5c54b3af2b6433fcdff1574e
fursini <francoursinikbw@gmail.com>
2024-09-27 15:36:51 +0200

    small fix

--- cpp9/ex02/PmergeMe.cpp
+++ cpp9/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: fursini <fursini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 22:36:23 by fursini           #+#    #+#             */
-/*   Updated: 2024/09/02 21:32:17 by fursini          ###   ########.fr       */
+/*   Updated: 2024/09/27 15:32:09 by fursini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,16 @@ void PmergeMe::printVector(std::vector<unsigned int> &vector) {
 	std::vector<unsigned int>::iterator it = vector.begin();
 	std::vector<unsigned int>::iterator ite = vector.end();
 
-	if (vector.size() > 10)
-		ite = vector.begin() + 10;
+	// if (vector.size() > 10)
+	// 	ite = vector.begin() + 10;
 	while (it != ite) {
 		std::cout << *it;
 		if (it + 1 != ite)
 			std::cout << " ";
 		it++;
 	}
-	if (vector.size() > 10)
-		std::cout << " [...]";
+	// if (vector.size() > 10)
+	// 	std::cout << " [...]";
 	std::cout << std::endl;
 }
 
@@ -160,17 +160,17 @@ void PmergeMe::printList(std::list<unsigned int> &list) {
 	std::list<unsigned int>::iterator it = list.begin();
 	std::list<unsigned int>::iterator ite = list.end();
 
-	if (list.size() > 10) {
-		ite = list.begin();
-		std::advance(ite, 10);
-	}
+	// if (list.size() > 10) {
+	// 	ite = list.begin();
+	// 	std::advance(ite, 10);
+	// }
 	while (it != ite) {
 		std::cout << *it;
 		if (++it != ite)
 			std::cout << " ";
 	}
-	if (list.size() > 10)
-		std::cout << " [...]";
+	// if (list.size() > 10)
+	// 	std::cout << " [...]";
 	std::cout << std::endl;
 }
 
@@ -183,26 +183,26 @@ void PmergeMe::execute(char **av) {
 		exit(1);
 	}
 	// Vector
-	start = clock();  // Start the timer
 	std::cout << "Before: ";
 	printVector(this->vector);
+	start = clock();
 	sortVector(this->vector);
+	end = clock();
 	std::cout << "After: ";
 	printVector(this->vector);
-	end = clock();  // End the timer
 	time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-	std::cout << std::fixed << std::setprecision(10) << "Time to process a range of " << this->vector.size() << " elements with std::vector: " << time << " s" << std::endl;
+	std::cout << std::fixed << std::setprecision(10) << "Time to process a range of " << this->vector.size() << " elements with std::vector: " << time * 1000 << "ms" << std::endl;
 
 	std::cout << std::endl;
 
 	// List
-	start = clock();  // Start the timer
 	std::cout << "Before: ";
 	printList(this->list);
+	start = clock();
 	sortList(this->list);
+	end = clock();
 	std::cout << "After: ";
 	printList(this->list);
-	end = clock();  // End the timer
 	time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-	std::cout << std::fixed << std::setprecision(10) << "Time to process a range of " << this->list.size() << " elements with std::list: " << time << " s" << std::endl;
+	std::cout << std::fixed << std::setprecision(10) << "Time to process a range of " << this->list.size() << " elements with std::list: " << time * 1000 << "ms" << std::endl;
 }
